e1f2519de2aefbc57fea79135de8c49219e0ebce
ohjinseo <ojs835@naver.com>
2022-02-02 16:38:37 +0900

    최근 수 식별, 방어, 공격 보완

--- omok/Form1.cs
+++ omok/Form1.cs
@@ -19,11 +19,12 @@ namespace omok
         const int limit = 2;
         const int INF = int.MaxValue;
         int aiX, aiY;
+        int whiteX, whiteY;
         Boolean pvp = false;
 
         //↗,→,↘, ↓
-        int[] omok_dirX = new int[]{ -1, 0, 1, 1 };
-        int[] omok_dirY = new int[]{ 1, 1, 1, 0 };
+        int[] omok_dirX = new int[] { -1, 0, 1, 1 };
+        int[] omok_dirY = new int[] { 1, 1, 1, 0 };
 
         //짝수는 ↗,→,↘, ↓
         //홀수는 ↑, ←, ↖, ↙ 
@@ -31,13 +32,15 @@ namespace omok
         int[] dirX = new int[] { 1, -1, 0, 0, 1, -1, -1, 1 };
         int[] dirY = new int[] { 0, 0, 1, -1, 1, -1, 1, -1 };
 
-        enum STONE { NONE, BLACK, WHITE};
+        enum STONE { NONE, BLACK, WHITE };
         STONE[,] board = new STONE[19, 19];
         bool flag = false;  //flase = 흑돌, true = 흰돌
+        bool isFirst = true;
 
         Graphics g;
         Pen pen;
         Brush wBrush, bBrush;
+        
 
         private void panel1_Paint(object sender, PaintEventArgs e)
         {
@@ -69,12 +72,12 @@ namespace omok
                       flowerSize, flowerSize);
                 }
             }
-    }
+        }
         public void init()
         {
-            for(int i = 0; i < 19; i++)
+            for (int i = 0; i < 19; i++)
             {
-                for(int k = 0; k < 19; k++)
+                for (int k = 0; k < 19; k++)
                 {
                     board[i, k] = STONE.NONE;
                 }
@@ -101,18 +104,36 @@ namespace omok
             int x = (e.X - margin + gridSize / 2) / gridSize;
             int y = (e.Y - margin + gridSize / 2) / gridSize;
 
-            if(x < 0 || y < 0 || x >= 19 || y >= 19)
+            Rectangle r = new Rectangle(
+                margin + gridSize * whiteY - stoneSize / 2,
+                margin + gridSize * whiteX - stoneSize / 2,
+                stoneSize, stoneSize);
+
+            Pen p = new Pen(Color.White, 3);
+
+            if (!isFirst)
+            {
+                g.FillEllipse(wBrush, r);
+                g.DrawEllipse(p, r);
+            }
+            
+
+            if (isFirst) isFirst = false;
+
+
+            if (x < 0 || y < 0 || x >= 19 || y >= 19)
             {
                 MessageBox.Show("테두리를 벗어났습니다.");
                 return;
             }
 
-            if (board[y,x] != STONE.NONE) return;
+            if (board[y, x] != STONE.NONE) return;
 
-            Rectangle r = new Rectangle(
+            r = new Rectangle(
                 margin + gridSize * x - stoneSize / 2,
                 margin + gridSize * y - stoneSize / 2,
                 stoneSize, stoneSize);
+
 
             if (pvp)
             {
@@ -135,10 +156,13 @@ namespace omok
             else
             {
                 //ai 모드
+               
+
                 board[y, x] = STONE.BLACK;
                 g.FillEllipse(bBrush, r);
                 if (isOmok(flag)) return;
 
+                
 
                 alphabetaPruning(0, int.MinValue, int.MaxValue);
 
@@ -147,13 +171,16 @@ namespace omok
                     margin + gridSize * aiX - stoneSize / 2,
                     stoneSize, stoneSize);
 
-
+                whiteX = aiX;
+                whiteY = aiY;
+                p = new Pen(Color.Red, 3);
                 g.FillEllipse(wBrush, r);
+                g.DrawEllipse(p, r);
                 board[aiX, aiY] = STONE.WHITE;
                 Console.WriteLine(aiX + " " + aiY);
                 if (isOmok(!flag)) return;
             }
-        }                       
+        }
 
         public Boolean isCorrectRange(int x, int y)
         {
@@ -171,11 +198,11 @@ namespace omok
                 int ny = y;
                 Boolean s = true;
 
-                for(int k = 0; k < 4; k++)
+                for (int k = 0; k < 4; k++)
                 {
                     nx += omok_dirX[i];
                     ny += omok_dirY[i];
-            
+
                     if (!isCorrectRange(nx, ny) || currentStone != board[nx, ny])
                     {
                         s = false;
@@ -193,15 +220,15 @@ namespace omok
         public Boolean isOmok(Boolean flag)
         {
             STONE currentStone = flag ? STONE.WHITE : STONE.BLACK;
-            
+
 
             for (int i = 0; i < 19; i++)
             {
-                for(int k = 0; k < 19; k++)
+                for (int k = 0; k < 19; k++)
                 {
-                    if(currentStone == board[i, k])
+                    if (currentStone == board[i, k])
                     {
-                        if(fourWaySearch(i, k, flag))
+                        if (fourWaySearch(i, k, flag))
                         {
                             MessageBox.Show(currentStone + "이 이겼습니다.");
                             return true;
@@ -213,10 +240,10 @@ namespace omok
             return false;
         }
 
-        
+
         public int alphabetaPruning(int depth, int alpha, int beta)
         {
-            if(depth == 3)
+            if (depth == 3)
             {
                 //Console.WriteLine(getStatus());
                 return getStatus();
@@ -229,24 +256,24 @@ namespace omok
 
                 for (int x = 0; x < 19; x++)    //for each child of node
                 {
-                    for(int y = 0; y < 19; y++)
+                    for (int y = 0; y < 19; y++)
                     {
                         STONE curStone = board[x, y];
                         Boolean flag = false;
 
                         //현재 좌표에 돌이 없을 시 주변에 돌이 있을 시 돌을 놓음
-                        if(curStone == STONE.NONE)
+                        if (curStone == STONE.NONE)
                         {
                             int nx, ny;
 
-                            for(int k = 0; k < 8; k++)
+                            for (int k = 0; k < 8; k++)
                             {
                                 nx = x + dirX[k];
                                 ny = y + dirY[k];
 
                                 if (!isCorrectRange(nx, ny)) continue;
 
-                                if(board[nx, ny] != STONE.NONE)
+                                if (board[nx, ny] != STONE.NONE)
                                 {
                                     flag = true;
                                     break;
@@ -272,9 +299,9 @@ namespace omok
                                 alpha = Math.Max(alpha, v);
 
                                 // 가지치기
-                                if(beta <= alpha)
+                                if (beta <= alpha)
                                 {
-                                    pruning = true; 
+                                    pruning = true;
                                     break;
                                 }
                             }
@@ -356,7 +383,7 @@ namespace omok
                     Boolean isOneSideBlock;
                     STONE curStone = board[x, y];
 
-                    
+
                     for (int k = 0; k < 8; k += 2)
                     {
                         if (!isFitFive(x, y, k)) continue;  //만약 양방향으로 오목이 들어가지 않는다면 무시
@@ -365,12 +392,12 @@ namespace omok
                         ny = y;
 
                         //돌의 개수를 확인하고 돌이 한 칸 띄워진지 확인함
-                         stoneCnt = 1;
-                         flag = false;
-                         isOneSpace = false;
-                         isOneSideBlock = false;
+                        stoneCnt = 1;
+                        flag = false;
+                        isOneSpace = false;
+                        isOneSideBlock = false;
 
-                       //상하를 제외하고 x, y가 보드 끝에 있으면 한 쪽이 막힌 것으로 간주
+                        //상하를 제외하고 x, y가 보드 끝에 있으면 한 쪽이 막힌 것으로 간주
                         if (k != 0 && (x == 0 || y == 0 || x == 18 || y == 18)) isOneSideBlock = true;
 
                         for (int i = 0; i < 4; i++)
@@ -462,36 +489,36 @@ namespace omok
                         int sum = 0;
 
                         //수가 1개일 때
-                        if(stoneCnt == 1)
+                        if (stoneCnt == 1)
                         {
                             //한 쪽이 막혔을 때
-                            if(isOneSideBlock && !isOneSpace)
+                            if (isOneSideBlock && !isOneSpace)
                             {
                                 sum += 5;
                             }
-                            
+
                             //안막혔을 때
-                            else if(!isOneSideBlock && !isOneSpace)
+                            else if (!isOneSideBlock && !isOneSpace)
                             {
                                 sum += 10;
                             }
                         }
 
                         //수가 2개일 때
-                        else if(stoneCnt == 2)
+                        else if (stoneCnt == 2)
                         {
                             //한 쪽이 막혔을 때
-                            if(isOneSideBlock && !isOneSpace)
+                            if (isOneSideBlock && !isOneSpace)
                             {
                                 sum += 30;
                             }
                             //한 쪽이 막히고 한 칸 띄워졌을 때
-                            else if(isOneSideBlock && isOneSpace)
+                            else if (isOneSideBlock && isOneSpace)
                             {
                                 sum += 15;
                             }
                             //안막혔을 때
-                            else if(!isOneSideBlock && !isOneSpace)
+                            else if (!isOneSideBlock && !isOneSpace)
                             {
                                 sum += 40;
                             }
@@ -500,11 +527,11 @@ namespace omok
                             {
                                 sum += 30;
                             }
-                            
+
                         }
 
                         //수가 3개일 때
-                        else if(stoneCnt == 3)
+                        else if (stoneCnt == 3)
                         {
                             //한 쪽이 막혔을 때
                             if (isOneSideBlock && !isOneSpace)
@@ -529,7 +556,7 @@ namespace omok
                         }
 
                         //수가 4개일 때
-                        else if(stoneCnt == 4)
+                        else if (stoneCnt == 4)
                         {
                             //한 쪽이 막혔을 때
                             if (isOneSideBlock && !isOneSpace)
@@ -553,79 +580,83 @@ namespace omok
                             }
                         }
 
+                        //수가 5개 또는 6개일 때
+                        else if(stoneCnt == 5 || stoneCnt == 6)
+                        {
+                            sum += 2000;
+                        }
+
                         if (curStone == STONE.BLACK) playerWeight += sum;
                         else aiWeight += sum;
                     }
-                    }
                 }
+            }
             //크다면 AI가 유리, 작다면 Player가 유리
             return aiWeight - playerWeight;
-            }
+        }
 
         //(x, y) 좌표를 받고 양방향으로 오목이 들어갈 공간이 있는지 확인
         Boolean isFitFive(int x, int y, int k)
+        {
+            STONE curStone = board[x, y];
+            int nx = x;
+            int ny = y;
+            int cnt = 0;
+
+            for (int i = 0; i < 4; i++)
             {
-                STONE curStone = board[x, y];
-                int nx = x;
-                int ny = y;
-                int cnt = 0;
-
-                for (int i = 0; i < 4; i++)
-                {
-                    //정방향 탐색
-                    nx += dirX[k];
-                    ny += dirY[k];
+                //정방향 탐색
+                nx += dirX[k];
+                ny += dirY[k];
 
 
-                    if (!isCorrectRange(nx, ny)) break;
+                if (!isCorrectRange(nx, ny)) break;
 
-                    if (board[nx, ny] == STONE.NONE || board[nx, ny] == curStone) cnt++;
-                    else break;
+                if (board[nx, ny] == STONE.NONE || board[nx, ny] == curStone) cnt++;
+                else break;
 
-                }
+            }
+
+            if (cnt == 4) return true;
+
+            nx = x;
+            ny = y;
+            for (int i = 0; i < 4; i++)
+            {
+                //역방향 탐색
+                nx += dirX[k + 1];
+                ny += dirY[k + 1];
+
+                if (!isCorrectRange(nx, ny)) break;
+
+                if (board[nx, ny] == STONE.NONE || board[nx, ny] == curStone) cnt++;
+                else break;
 
                 if (cnt == 4) return true;
-
-                nx = x;
-                ny = y;
-                for (int i = 0; i < 4; i++)
-                {
-                    //역방향 탐색
-                    nx += dirX[k + 1];
-                    ny += dirY[k + 1];
-
-                    if (!isCorrectRange(nx, ny)) break;
-
-                    if (board[nx, ny] == STONE.NONE || board[nx, ny] == curStone) cnt++;
-                    else break;
-
-                    if (cnt == 4) return true;
-                }
-                return false;
             }
+            return false;
+        }
 
-            public Form1()
-            {
-                InitializeComponent();
+        public Form1()
+        {
+            InitializeComponent();
 
 
-                this.Text = "OMOK";
-                BackColor = Color.Orange;
+            this.Text = "OMOK";
+            BackColor = Color.Orange;
 
-                pen = new Pen(Color.Black);
-                bBrush = new SolidBrush(Color.Black);
-                wBrush = new SolidBrush(Color.White);
+            pen = new Pen(Color.Black);
+            bBrush = new SolidBrush(Color.Black);
+            wBrush = new SolidBrush(Color.White);
 
-                this.ClientSize = new Size(2 * margin + 18 * gridSize, 2 * margin + 18 * gridSize);
-            }
+            this.ClientSize = new Size(2 * margin + 18 * gridSize, 2 * margin + 18 * gridSize);
+        }
     }
 }
 
 
 /*
-
 isFitFive 테스트
-
 for (int i = 0; i < 8; i += 2)
 {
     if (isFitFive(x, y, i))
@@ -637,11 +668,7 @@ for (int i = 0; i < 8; i += 2)
         Console.WriteLine("------------ " + i + "번 쨰 5개가 들어갈 수 없습니다");
     }
 }
-
-
-
 //board 좌표 테스트
-
 for (int i = 0; i < 10; i++)
 {
     for (int k = 0; k < 10; k++)
@@ -651,17 +678,11 @@ for (int i = 0; i < 10; i++)
     Console.WriteLine();
 }
 Console.WriteLine(); Console.WriteLine();
-
-
-
-
 //status 함수 테스트
-
 Console.WriteLine(curStone + " -> " + "(" + x + ", " + y + ") " + k + "방향 입니다. ------------------------");
 Console.WriteLine("총 돌의 개수는 ? : " + stoneCnt);
 Console.WriteLine("한 칸 띄워져 있는지 ? : " + isOneSpace);
 Console.WriteLine("한 쪽이 막혀 있는지? : " + isOneSideBlock);
 Console.WriteLine();
 Console.WriteLine();
-
 */
